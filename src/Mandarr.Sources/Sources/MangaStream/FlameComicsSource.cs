741fa354ca26f960a793e7fa3d584b3b38f068af
Christiaan Bloemendaal <github@tnrd.net>
2023-12-28 16:23:53 +0100

    feat: add FlameComics

---
+++ src/Mandarr.Sources/Sources/MangaStream/FlameComicsSource.cs
@@ -0,0 +1,17 @@
+﻿using Injectio.Attributes;
+using Mandarr.Sources.Clients;
+
+namespace Mandarr.Sources.Sources.MangaStream;
+
+[RegisterSingleton<ISource>(Duplicate = DuplicateStrategy.Append)]
+internal class FlameComicsSource : MangaStreamSourceBase
+{
+    protected override string Id => "flamecomics";
+    protected override string Name => "Flame Comics";
+    protected override string Url => "https://flamecomics.com";
+
+    public FlameComicsSource(GenericHttpClient genericHttpClient, CloudflareHttpClient cloudflareHttpClient)
+        : base(genericHttpClient, cloudflareHttpClient)
+    {
+    }
+}
