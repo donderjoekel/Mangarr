9af1b302d5723e0c597b16a3a83b2c9566f25443
Christiaan Bloemendaal <github@tnrd.net>
2023-12-31 19:39:03 +0100

    feat: add caching of network requests

---
+++ src/Mangarr.Backend/Sources/Caching/CachedResponseMessage.cs
@@ -0,0 +1,30 @@
+﻿namespace Mangarr.Backend.Sources.Caching;
+
+public class CachedResponseMessage
+{
+    public HttpResponseMessage ResponseMessage { get; set; }
+    public byte[]? Content { get; set; }
+
+    public CachedResponseMessage(HttpResponseMessage responseMessage) => ResponseMessage = responseMessage;
+
+    public async Task SaveContent()
+    {
+        // ReSharper disable once ConditionIsAlwaysTrueOrFalseAccordingToNullableAPIContract
+        if (ResponseMessage.Content == null)
+        {
+            return;
+        }
+
+        Content = await ResponseMessage.Content.ReadAsByteArrayAsync();
+    }
+
+    public HttpResponseMessage ToHttpResponseMessage()
+    {
+        if (Content != null)
+        {
+            ResponseMessage.Content = new ByteArrayContent(Content);
+        }
+
+        return ResponseMessage;
+    }
+}
--- src/Mangarr.Backend/Sources/Extensions/HostingExtensions.cs
+++ src/Mangarr.Backend/Sources/Extensions/HostingExtensions.cs
@@ -1,4 +1,6 @@
 ﻿using Mangarr.Backend.Configuration;
+using Mangarr.Backend.Services;
+using Mangarr.Backend.Sources.Caching;
 using Mangarr.Backend.Sources.Clients;
 using Mangarr.Backend.Sources.Cloudflare;
 using Microsoft.Extensions.Options;
@@ -15,13 +17,23 @@ public static class HostingExtensions
 
     public static IServiceCollection AddSources(this IServiceCollection services)
     {
-        services.AddHttpClient("Generic").AddRetryPolicy();
+        services.AddTransient<CachingHandler>(provider =>
+            new CachingHandler(provider.GetRequiredService<CachingService>()));
+
+        services.AddHttpClient("Generic")
+            .AddRetryPolicy()
+            .AddHttpMessageHandler<CachingHandler>();
+
         services.AddTransient<CustomClearanceHandler>(provider =>
         {
             FlareSolverrOptions options = provider.GetRequiredService<IOptions<FlareSolverrOptions>>().Value;
             return new CustomClearanceHandler(options.Host);
         });
-        services.AddHttpClient("Cloudflare").AddHttpMessageHandler<CustomClearanceHandler>().AddRetryPolicy();
+
+        services.AddHttpClient("Cloudflare")
+            .AddRetryPolicy()
+            .AddHttpMessageHandler<CachingHandler>()
+            .AddHttpMessageHandler<CustomClearanceHandler>();
 
         services.AddMangarrBackend();
         return services;
