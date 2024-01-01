78944197ba5a6ca50f8a14378e489b88c92abe71
Christiaan Bloemendaal <github@tnrd.net>
2024-01-01 16:05:38 +0100

    feat: add advanced search for custom search queries

--- src/Mangarr.Frontend/Pages/Manga/Link/Content.razor.cs
+++ src/Mangarr.Frontend/Pages/Manga/Link/Content.razor.cs
@@ -1,19 +1,20 @@
 ﻿using FluentResults;
+using Mangarr.Frontend.Api;
 using Mangarr.Shared.Models;
 using Mangarr.Shared.Responses;
-using Mangarr.Frontend.Api;
 using Microsoft.AspNetCore.Components;
 
 namespace Mangarr.Frontend.Pages.Manga.Link;
 
 public partial class Content
 {
-    [Parameter] public string Id { get; set; }
-    [Inject] public BackendApi BackendApi { get; set; } = null!;
-
     private readonly List<ProviderModel> _items = new();
+    private CancellationTokenSource _cancellationTokenSource = new();
 
     private bool _isSearching;
+    [Parameter] public string Id { get; set; }
+    [Parameter] public string? CustomSearchQuery { get; set; }
+    [Inject] public BackendApi BackendApi { get; set; } = null!;
 
     protected override void OnInitialized() => SearchAsync();
 
@@ -33,6 +34,41 @@ public partial class Content
             _items.Clear();
             _items.AddRange(result.Value.Data.Where(x => x.Enabled).OrderBy(x => x.Name));
         }
+
+        _isSearching = false;
+        await InvokeAsync(StateHasChanged);
+    }
+
+    private Task SearchQueryChanged(ChangeEventArgs args)
+    {
+        string query = args.Value?.ToString() ?? string.Empty;
+        _cancellationTokenSource.Cancel();
+        _cancellationTokenSource = new CancellationTokenSource();
+        return UpdateSearch(query, _cancellationTokenSource.Token);
+    }
+
+    private async Task UpdateSearch(string query, CancellationToken ct)
+    {
+        _isSearching = true;
+        await InvokeAsync(StateHasChanged);
+
+        // Capture the cancellation of the search in case other keys are pressed
+        try
+        {
+            await Task.Delay(500, ct);
+        }
+        catch (TaskCanceledException)
+        {
+            return;
+        }
+
+        // In case we're slipping through
+        if (ct.IsCancellationRequested)
+        {
+            return;
+        }
+
+        CustomSearchQuery = query;
 
         _isSearching = false;
         await InvokeAsync(StateHasChanged);
--- src/Mangarr.Frontend/Pages/Manga/Link/ContentSource.razor.cs
+++ src/Mangarr.Frontend/Pages/Manga/Link/ContentSource.razor.cs
@@ -1,31 +1,78 @@
 ﻿using FluentResults;
+using Mangarr.Frontend.Api;
 using Mangarr.Shared.Models;
 using Mangarr.Shared.Responses;
-using Mangarr.Frontend.Api;
 using Microsoft.AspNetCore.Components;
 
 namespace Mangarr.Frontend.Pages.Manga.Link;
 
 public partial class ContentSource
 {
+    private readonly List<ProviderMangaModel> _items = new();
+    private string? _customSearchQuery;
+    private bool _isSearching;
     [Parameter] public string Id { get; set; }
+    [Parameter] public string? CustomSearchQuery { get; set; }
     [Parameter] public ProviderModel Item { get; set; } = null!;
 
     [Inject] public BackendApi BackendApi { get; set; } = null!;
-
-    private readonly List<ProviderMangaModel> _items = new();
-    private bool _isSearching;
 
     private string Identifier => Item.Identifier;
     private string Title => Item.Name;
 
     protected override void OnInitialized() => SearchAsync();
 
+    protected override void OnParametersSet()
+    {
+        if (CustomSearchQuery != _customSearchQuery)
+        {
+            SearchAsync();
+            _customSearchQuery = CustomSearchQuery;
+        }
+    }
+
     private async void SearchAsync()
     {
         _isSearching = true;
         await InvokeAsync(StateHasChanged);
 
+        if (!string.IsNullOrEmpty(CustomSearchQuery))
+        {
+            await SearchByCustom();
+        }
+        else
+        {
+            await SearchById();
+        }
+
+        _isSearching = false;
+        await InvokeAsync(StateHasChanged);
+    }
+
+    private async Task SearchByCustom()
+    {
+        Result<ProviderSearchResponse> result = await BackendApi.SearchProvider(Item.Identifier, CustomSearchQuery);
+
+        if (result.IsFailed)
+        {
+            // TODO: Log error
+        }
+        else
+        {
+            foreach (ProviderMangaModel providerMangaModel in result.Value.Data)
+            {
+                if (_items.Any(x => x.Id == providerMangaModel.Id))
+                {
+                    continue;
+                }
+
+                _items.Add(providerMangaModel);
+            }
+        }
+    }
+
+    private async Task SearchById()
+    {
         if (!int.TryParse(Id, out int id))
         {
             // TODO: Log error
@@ -65,8 +112,5 @@ public partial class ContentSource
                 }
             }
         }
-
-        _isSearching = false;
-        await InvokeAsync(StateHasChanged);
     }
 }
