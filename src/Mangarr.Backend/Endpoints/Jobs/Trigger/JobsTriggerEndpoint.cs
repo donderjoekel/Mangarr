99a4fcc3c6626b20b8163eca46b6f4146b03a381
Christiaan Bloemendaal <github@tnrd.net>
2024-01-02 10:56:41 +0100

    feat(backend); add endpoint for scheduling trigger

---
+++ src/Mangarr.Backend/Endpoints/Jobs/Trigger/JobsTriggerEndpoint.cs
@@ -0,0 +1,34 @@
+﻿using Mangarr.Shared.Requests;
+using Mangarr.Shared.Responses;
+using Quartz;
+
+namespace Mangarr.Backend.Endpoints.Jobs.Trigger;
+
+public class JobsTriggerEndpoint : Endpoint<JobsTriggerRequest, JobsTriggerResponse>
+{
+    private readonly ISchedulerFactory _schedulerFactory;
+
+    public JobsTriggerEndpoint(ISchedulerFactory schedulerFactory) => _schedulerFactory = schedulerFactory;
+
+    public override void Configure()
+    {
+        Post("/jobs/{group}/{name}/trigger");
+        AllowAnonymous();
+    }
+
+    public override async Task HandleAsync(JobsTriggerRequest req, CancellationToken ct)
+    {
+        IScheduler scheduler = await _schedulerFactory.GetScheduler(ct);
+
+        ITrigger? trigger = await scheduler.GetTrigger(new TriggerKey(req.Name, req.Group), ct);
+
+        if (trigger == null)
+        {
+            await SendNotFoundAsync(ct);
+            return;
+        }
+
+        await scheduler.ScheduleJob(trigger, ct);
+        await SendOkAsync(ct);
+    }
+}
---
+++ src/Mangarr.Shared/Requests/JobsTriggerRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace Mangarr.Shared.Requests;
+
+public class JobsTriggerRequest
+{
+    public string Group { get; set; }
+    public string Name { get; set; }
+}
