f779c54bab6d810170c9cc087ecaa6aea0d2cb18
Christiaan Bloemendaal <github@tnrd.net>
2024-02-04 22:53:20 +0100

    feat: add refreshing of activity page

--- src/Mangarr.Stack/Pages/Activity/Activity.razor.cs
+++ src/Mangarr.Stack/Pages/Activity/Activity.razor.cs
@@ -1,21 +1,36 @@
 ﻿using Mangarr.Stack.Database.Documents;
 using Mangarr.Stack.Database.Repositories;
 using Microsoft.AspNetCore.Components;
+using Timer = System.Timers.Timer;
 
 namespace Mangarr.Stack.Pages.Activity;
 
-public partial class Activity
+public sealed partial class Activity : IDisposable
 {
     private readonly List<ChapterProgressDocument> _items = new();
+    private Timer _timer = null!;
 
     [Inject] public ChapterProgressRepository Repository { get; set; } = null!;
 
-    protected override void OnInitialized() => Refresh();
+    void IDisposable.Dispose()
+    {
+        _timer.Stop();
+        _timer.Dispose();
+    }
+
+    protected override void OnInitialized()
+    {
+        _timer = new Timer(2500);
+        _timer.Elapsed += (sender, args) => Refresh();
+        _timer.Start();
+
+        Refresh();
+    }
 
     private void Refresh()
     {
         _items.Clear();
-        _items.AddRange(Repository.Items.OrderByDescending(x => x.IsActive).ThenBy(x => x.DateCreated));
+        _items.AddRange(Repository.Items.OrderByDescending(x => x.IsActive).ThenBy(x => x.DateCreated).ToList());
         InvokeAsync(StateHasChanged);
     }
 }
