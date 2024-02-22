0187679f1c15d603e9c164d15192f09614a822fb
Christiaan Bloemendaal <github@tnrd.net>
2024-02-22 21:33:50 +0100

    feat: add recycling of chapters when they fail

---
+++ src/Mangarr.Stack/Pages/Activity/ActivityItem.razor.cs
@@ -0,0 +1,19 @@
+﻿using Mangarr.Stack.Database.Documents;
+using Mangarr.Stack.Database.Repositories;
+using Microsoft.AspNetCore.Components;
+
+namespace Mangarr.Stack.Pages.Activity;
+
+public partial class ActivityItem
+{
+    [Inject] public ChapterProgressRepository ChapterProgressRepository { get; set; } = null!;
+    [Parameter] public ChapterProgressDocument Item { get; set; } = null!;
+    [Parameter] public int Index { get; set; }
+    [Parameter] public EventCallback OnDelete { get; set; }
+
+    private async Task Recycle()
+    {
+        await ChapterProgressRepository.DeleteAsync(Item);
+        await OnDelete.InvokeAsync();
+    }
+}
